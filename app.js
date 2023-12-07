799d6553bdcc780260438bb0a677c9444abf9daf
Aluz <aluzdev@gmail.com>
2023-12-07 00:30:00 -0700

    purified input

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const fs = require("fs");
 
 function solve(puzzle) {
-  const noCardText = /Card \d+:/;
+  const noCardText = /Card \d+:\s+/;
+  let winningNumbers = [];
+  let myNumbers = [];
 
-  puzzle.split("\n").forEach((element) => {
-    let winningNumbersAndMyNumbers = element
-      .replace(noCardText, "")
+  puzzle.split("\n").forEach((row) => {
+    let winningNumbersAndMyNumbers = row
       .trim()
+      .replace(noCardText, "")
       .split(" | ");
-    let [winningNumbers, myNumbers] = winningNumbersAndMyNumbers;
-    console.log({ winningNumbers, myNumbers });
+    let [a, b] = winningNumbersAndMyNumbers;
+    winningNumbers.push(a?.split(" ").filter((token) => token != ""));
+    myNumbers.push(b?.split(" ").filter((token) => token != ""));
   });
-  return -1;
+  console.log({ winningNumbers, myNumbers });
 }
 
 fs.readFile("./mini.txt", "utf8", (err, data) => {
