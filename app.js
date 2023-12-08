80f2a357281e7cd2034f7eef895f19fb91db5959
Aluz <aluzdev@gmail.com>
2023-12-07 17:05:00 -0700

    puzzle 4 solved

--- app.js
+++ app.js
@@ -1,51 +1,11 @@
 const fs = require("fs");
 
-function transformInputIntoArrayOfWinningNumbersAndMyNumbers(puzzle) {
-  const noCardText = /Card \d+:\s+/;
-  let winCards = [];
-  let myCards = [];
-  puzzle.split("\n").forEach((row) => {
-    let winningNumbersAndMyNumbers = row
-      .trim()
-      .replace(noCardText, "")
-      .split(" | ");
-    let [a, b] = winningNumbersAndMyNumbers;
-    winCards.push(
-      a?.split(" ").filter((token) => token !== "" && token !== undefined)
-    );
-    myCards.push(
-      b?.split(" ").filter((token) => token != "" && token !== undefined)
-    );
-  });
-  return [winCards, myCards];
-}
-
-function solve(puzzle) {
-  const [winningNumbers, myNumbers] =
-    transformInputIntoArrayOfWinningNumbersAndMyNumbers(puzzle);
-  let totalPoints = 0;
-
-  for (let i = 0; i < myNumbers.length; i++) {
-    let countOfMyWinningNumbers = 0;
-    for (let j = 0; j < myNumbers[i]?.length; j++) {
-      let num = myNumbers[i][j];
-      let setOfWinningNumbers = new Set(winningNumbers[i]);
-      if (setOfWinningNumbers.has(num)) {
-        countOfMyWinningNumbers++;
-      }
-    }
-    let totalCardPoints = Math.floor(Math.pow(2, countOfMyWinningNumbers - 1));
-
-    totalPoints += totalCardPoints;
-  }
-  return totalPoints;
-}
-
-fs.readFile("./big.txt", "utf8", (err, data) => {
+fs.readFile("./puzzles/p4_big.txt", "utf8", (err, data) => {
   if (err) {
     console.error(err);
     return;
   }
-  const solution = solve(data);
+  const puzzleSolver = require("./solutions/puzzle4");
+  const solution = puzzleSolver(data);
   console.log(solution);
 });
