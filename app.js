90c7fc1b5980088b8b7ed3a362d3a7955c71098d
aluzdev <aluzdev@gmail.com>
2023-12-07 17:58:55 -0700

    Puzzle4 solution
    https://adventofcode.com/2023/day/4

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
 const fs = require("fs");
 
-function solve(puzzle) {
-  puzzle.split("\n").forEach((element) => {
-    console.log(element.split(" | "));
-    element.split(" | ");
-  });
-  return -1;
-}
-
-fs.readFile("./mini.txt", "utf8", (err, data) => {
+fs.readFile("./puzzles/p4_big.txt", "utf8", (err, data) => {
   if (err) {
     console.error(err);
     return;
   }
-  const solution = solve(data);
-  console.log(solution); // Output: 4361
+  const puzzleSolver = require("./solutions/puzzle4");
+  const solution = puzzleSolver(data);
+  console.log(solution);
 });
